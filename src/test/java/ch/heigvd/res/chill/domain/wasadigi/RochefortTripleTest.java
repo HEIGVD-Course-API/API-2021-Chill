30de4c014eb180f73634403f14acaade37427ce6
Androto9 <71312332+Androto9@users.noreply.github.com>
2021-09-29 11:38:11 +0200

    Add rochefort triple

---
+++ src/test/java/ch/heigvd/res/chill/domain/wasadigi/RochefortTripleTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.wasadigi;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RochefortTripleTest {
+
+  @Test
+  void thePriceAndNameForBoxerShouldBeCorrect() {
+    RochefortTriple beer = new RochefortTriple();
+    assertEquals(beer.getName(), RochefortTriple.NAME);
+    assertEquals(beer.getPrice(), RochefortTriple.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForBoxer() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.res.chill.domain.wasadigi.RochefortTriple";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = RochefortTriple.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
