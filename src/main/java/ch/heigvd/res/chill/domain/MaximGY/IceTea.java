8cc34e884d1e67b540e13cb0cc9ce824fef041d7
Maxim Golay <maxim.golay@ik.me>
2021-09-30 12:13:22 +0200

    Implemented Icetea + Tests

--- src/main/java/ch/heigvd/res/chill/domain/MaximGY/IceTea.java
+++ src/main/java/ch/heigvd/res/chill/domain/MaximGY/IceTea.java
@@ -1,2 +1,21 @@
-package ch.heigvd.res.chill.domain.MaximGY;public class IceTea {
+package ch.heigvd.res.chill.domain.MaximGY;
+
+import ch.heigvd.res.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class IceTea implements IProduct {
+
+    public static final String NAME = "IceTea";
+    public static final BigDecimal PRICE = new BigDecimal(1.8);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
 }
