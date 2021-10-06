5a2864db6032b80460ef301229ae2f5b05b59c5e
Jean-François <jeff@mbp-404.local>
2021-10-06 19:03:41 +0200

    bartender reply test

--- src/test/java/ch/heigvd/res/chill/domain/JFPasche/PaleAle26Test.java
+++ src/test/java/ch/heigvd/res/chill/domain/JFPasche/PaleAle26Test.java
@@ -1,6 +1,12 @@
 package ch.heigvd.res.chill.domain.JFPasche;
 
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.domain.jorisSchaller.Kekette;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,5 +16,15 @@ class PaleAle26Test {
         PaleAle26 beer = new PaleAle26();
         assertEquals(beer.getName(), PaleAle26.NAME);
         assertEquals(beer.getPrice(), PaleAle26.PRICE);
+    }
+    @Test
+    void aBartenderShouldAcceptAnOrderForPaleAle26() {
+        int quantity = 4;
+        Bartender jeff = new Bartender();
+        String productName = "ch.heigvd.res.chill.domain.JFPasche.PaleAle26";
+        OrderRequest request = new OrderRequest(quantity, productName);
+        OrderResponse response = jeff.order(request);
+        BigDecimal expectedTotalPrice = PaleAle26.PRICE.multiply(new BigDecimal(quantity));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
     }
 }
