7b602a69fd5b0219d8bcd6ff68ec5809647a07a0
HugoMendes98 <mendeshugo98@gmail.com>
2022-02-22 21:09:07 +0100

    Ice Tea (lemon) added

---
+++ src/test/java/ch/heigvd/api/chill/domain/hugomendes98/IceTeaLemonTest.java
@@ -0,0 +1,32 @@
+package ch.heigvd.api.chill.domain.hugomendes98;
+
+import ch.heigvd.api.chill.domain.Bartender;
+import ch.heigvd.api.chill.domain.wasadigi.PunkIPA;
+import ch.heigvd.api.chill.protocol.OrderRequest;
+import ch.heigvd.api.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IceTeaLemonTest {
+
+    @Test
+    void thePriceAndNameForPunkIPAShouldBeCorrect() {
+        IceTeaLemon ice = new IceTeaLemon();
+        assertEquals(ice.getName(), IceTeaLemon.NAME);
+        assertEquals(ice.getPrice(), IceTeaLemon.PRICE);
+    }
+
+    @Test
+    void aBartenderShouldAcceptAnOrderForPunkIPA() {
+        Bartender jane = new Bartender();
+        String productName = "ch.heigvd.api.chill.domain.hugomendes98.IceTeaLemon";
+        OrderRequest request = new OrderRequest(3, productName);
+        OrderResponse response = jane.order(request);
+        BigDecimal expectedTotalPrice = IceTeaLemon.PRICE.multiply(new BigDecimal(3));
+        assertEquals(expectedTotalPrice, response.getTotalPrice());
+    }
+
+}
