1cce7493635b0fffb132e44e8aec1b508a857f03
Kitsunemma <yecomte@gmail.com>
2022-02-22 21:18:15 +0100

    add The Piou shaker

---
+++ src/test/java/ch/heigvd/api/chill/domain/Kitsunemma/PiouShakerTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.api.chill.domain.Kitsunemma;
+
+import ch.heigvd.api.chill.domain.Bartender;
+import ch.heigvd.api.chill.protocol.OrderRequest;
+import ch.heigvd.api.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PiouShakerTest {
+
+  @Test
+  void thePriceAndNameForPunkIPAShouldBeCorrect() {
+    PiouShaker beer = new PiouShaker();
+    assertEquals(beer.getName(), PiouShaker.NAME);
+    assertEquals(beer.getPrice(), PiouShaker.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForPunkIPA() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.api.chill.domain.Kitsunemma.PiouShaker";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = PiouShaker.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
