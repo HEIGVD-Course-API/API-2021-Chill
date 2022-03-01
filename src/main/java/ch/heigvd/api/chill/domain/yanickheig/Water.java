f39e4de4cd603f2af87e245732877f38a26b9075
YanickHEIG <yanick.thomann@heig-vd.ch>
2022-03-01 20:23:53 +0100

    corrected unit test names

--- src/main/java/ch/heigvd/api/chill/domain/yanickheig/Water.java
+++ src/main/java/ch/heigvd/api/chill/domain/yanickheig/Water.java
@@ -1,2 +1,21 @@
-package ch.heigvd.api.chill.domain.yanickheig;public class Water {
+package ch.heigvd.api.chill.domain.yanickheig;
+
+import ch.heigvd.api.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Water implements IProduct {
+
+    public final static String NAME = "Water";
+    public final static BigDecimal PRICE = new BigDecimal(1.5);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
 }
